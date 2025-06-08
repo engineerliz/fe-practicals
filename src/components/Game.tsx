f3ec114156227b9315400f825907bf6cfd964ba4
liz <liz@atomism.co>
2025-06-08 16:04:21 -0700

    Game that made me almost kill myself

---
+++ src/components/Game.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { WINNING_COMBOS } from "../constants/winningCombos";
+import Tile, { tileValue } from "./Tile";
+
+export type player = "x" | "o";
+
+export default function Game() {
+  const [currentPlayer, setCurrentPlayer] = useState<player>("x");
+  const [board, setBoard] = useState<tileValue[]>([
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+  ]);
+
+  const getTileValue = (n: number) => {
+    return board[n];
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "row",
+        width: 650,
+      }}>
+      {board?.map((row, n) => (
+        <Tile
+          index={n}
+          value={getTileValue(n)}
+          onClick={function (n) {
+            const boardValue = board[n];
+            if (boardValue === "empty") {
+              let copy = [...board];
+
+              copy[n] = currentPlayer;
+
+              setBoard(copy);
+              for (const combo of WINNING_COMBOS) {
+                console.log([copy[combo[0]], copy[combo[1]], copy[combo[2]]]);
+                console.log("currentPlayer", currentPlayer);
+
+                const match =
+                  copy[combo[0]] === currentPlayer &&
+                  copy[combo[1]] === currentPlayer &&
+                  copy[combo[2]] === currentPlayer;
+
+                if (match) {
+                  alert(`Winner: ${currentPlayer}`);
+                  return;
+                } else {
+                  setCurrentPlayer(currentPlayer === "x" ? "o" : "x");
+                }
+              }
+            }
+          }}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ src/components/Tile.tsx
@@ -0,0 +1,28 @@
+import { player } from "./Game";
+
+export type tileValue = player | "empty";
+
+interface TileProps {
+  value?: tileValue;
+  index: number;
+  onClick: (n: number) => void;
+}
+
+export default function Tile({ value, index, onClick }: TileProps) {
+  return (
+    <div
+      onClick={() => {
+        onClick(index);
+      }}
+      style={{
+        border: "1px solid black",
+        height: 200,
+        width: 200,
+        background: "yellow",
+      }}>
+      {/* {index} */}
+      <br /> {value === "x" && <span>x</span>}
+      {value === "o" && <span>o</span>}
+    </div>
+  );
+}
