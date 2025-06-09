5faa947bc369bdf7d5ae381b0a10c30175d8ccd4
liz <liz@atomism.co>
2025-06-09 12:12:23 -0700

    Add infinite scroll for search results

--- src/searchDebounce/index.tsx
+++ src/searchDebounce/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SEARCH_RESULTS } from "./results";
 import { SearchResult } from "./searchResultModel";
 
@@ -6,6 +6,7 @@ export default function SearchDebounce() {
   const [results, setResults] = useState<SearchResult[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>();
   const [debouncedQuery, setDebouncedQuery] = useState<string>();
+  const resultsContainerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const handler = setTimeout(() => {
@@ -30,7 +31,39 @@ export default function SearchDebounce() {
   };
 
   const mockSearchApi = () => {
-    return SEARCH_RESULTS;
+    return SEARCH_RESULTS.slice(0, 5);
+  };
+
+  const getPaginatedSearchResults = async (): Promise<SearchResult[]> => {
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        const endIndex = results.length - 1;
+
+        // Get next results
+        let newEndIndex = endIndex + 2;
+        if (newEndIndex > SEARCH_RESULTS.length - 1) {
+          newEndIndex = SEARCH_RESULTS.length - 1;
+        }
+
+        // End of search results
+        if (endIndex >= newEndIndex) return [];
+        const nextPage = SEARCH_RESULTS.slice(endIndex, newEndIndex);
+        resolve(nextPage);
+      }, 300)
+    );
+  };
+
+  const handleScroll = () => {
+    const container = resultsContainerRef.current;
+    if (!container) return;
+
+    const { scrollHeight, scrollTop, clientHeight } = container;
+
+    if (clientHeight + scrollTop >= scrollHeight - 10) {
+      getPaginatedSearchResults().then((nextPage) => {
+        setResults([...results, ...nextPage]);
+      });
+    }
   };
 
   return (
@@ -44,7 +77,15 @@ export default function SearchDebounce() {
         <input onChange={onSearch} />
         <button>Search</button>
       </div>
-      <div>
+      <div
+        ref={resultsContainerRef}
+        onScroll={handleScroll}
+        style={{
+          height: 200,
+          border: "1px solid gray",
+          overflow: "scroll",
+          marginTop: 20,
+        }}>
         {results.map((item) => (
           <div key={item.id}>
             <h3>{item.title}</h3>
