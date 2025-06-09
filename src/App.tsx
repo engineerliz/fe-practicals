8173677a44f6461f054254668239106281595d93
liz <liz@atomism.co>
2025-06-09 11:33:37 -0700

    Add folders for each practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./Home";
 import TicTacToeGame from "./tictactoe/components/TicTacToeGame";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/tictactoe" element={<TicTacToeGame />} />
       </Routes>
-      <Link to="/tictactoe">
-        <button>Play Tic Tac Toe</button>
-      </Link>
     </BrowserRouter>
   );
 }
